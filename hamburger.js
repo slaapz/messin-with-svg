f4ef514cd7129d2781e16551dbd04a4bf076cd09
Patrick Ball <slaapz@gmail.com>
2018-12-20 22:20:45 -0700

    Added repo files

---
+++ hamburger.js
@@ -0,0 +1,57 @@
+const hamburger = document.querySelector('.hamburger');
+const lineOne = hamburger.querySelector('.line-one');
+const lineTwo = hamburger.querySelector('.line-two');
+const lineThree = hamburger.querySelector('.line-three');
+
+const lines = [lineOne, lineTwo, lineThree];
+const tlm = new TimelineMax({});
+
+const toggleMenu = new TimelineMax({
+  paused: true,
+  reversed: true
+});
+
+hamburger.addEventListener('mouseenter', () => {
+  if (hamburger.classList.contains('js-x')) {
+    return;
+  }
+  tlm.staggerTo(
+    lines,
+    0.25,
+    {
+      scaleX: 1.5,
+      repeat: 1,
+      yoyo: true,
+      ease: Power2.easeInOut,
+      svgOrigin: '50 50'
+    },
+    0.125
+  );
+});
+
+toggleMenu
+  .to(lineTwo, 0.125, { scaleX: 0 }, 0)
+  .to(
+    lineOne,
+    0.25,
+    { transformOrigin: '50% 50%', y: 8, ease: Power2.easeInOut },
+    'slide'
+  )
+  .to(
+    lineThree,
+    0.25,
+    {
+      transformOrigin: '50% 50%',
+      y: -8,
+      ease: Power2.easeInOut
+    },
+    'slide'
+  )
+  .to(hamburger, 0.5, { rotation: 360, ease: Power4.easeInOut })
+  .to(lineOne, 0.25, { rotation: 45, ease: Power2.easeInOut }, 'cross')
+  .to(lineThree, 0.25, { rotation: -45, ease: Power2.easeInOut }, 'cross');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('js-x');
+  toggleMenu.reversed() ? toggleMenu.play() : toggleMenu.reverse();
+});
